ff1f7c758b91a934b8c86c4efac09f26c43d09e0
= <luccashiroshi@gmail.com>
2022-12-22 12:28:13 -0300

    change colors

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function doGrid() {
     let columns = Math.trunc(window.innerWidth / 45)
     let lines = Math.trunc(window.innerHeight / 45)
 
-    for(var i = 1; i < lines; i++) {
+    for (var i = 1; i < lines; i++) {
         doColumns(columns)
     }
 }
@@ -12,7 +12,27 @@ function doColumns(number) {
         const block = document.createElement("div")
         block.classList.add("block")
         document.getElementById('container').appendChild(block)
+        block.onmouseover = (e) => {
+            block.style.transition = '70ms'
+            let color = getRandomColor()
+            block.style.backgroundColor = color
+            block.style.boxShadow = `0px 0px 50px 18px ${color}`
+        }
+        block.onmouseout = (e) => {
+            block.style.transition = '3s'
+            block.style.backgroundColor = ''
+            block.style.boxShadow = ''
+        }
     }
+}
+
+const getRandomColor = () => {
+    const maxVal = 0xFFFFFF
+    let randomNumber = Math.random() * maxVal
+    randomNumber = Math.floor(randomNumber)
+    randomNumber = randomNumber.toString(16)
+    let randomColor = randomNumber.padStart(6, 0)
+    return `#${randomColor.toUpperCase()}`
 }
 
 doGrid()
